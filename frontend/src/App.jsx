1a160c5bd755603e66a71b1c9f8848bfee868063
iman-123-coder <imanabbasi101@gmail.com>
2026-05-15 21:52:31 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,14 +11,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+
+        {/* Login page opens first */}
+        <Route path="/" element={<Login />} />
+
+        {/* Other pages */}
         <Route path="/home" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminPanel />} />
+
       </Routes>
     </BrowserRouter>
   )
