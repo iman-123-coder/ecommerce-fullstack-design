24f9cab8eb0324c6d3813aea58daa18233090ebc
iman-123-coder <imanabbasi101@gmail.com>
2026-05-15 16:34:10 +0500

    Part-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,11 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/products" element={<ProductListing />} />
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
