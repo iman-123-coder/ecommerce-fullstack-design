c859abfc2d8e899f531cf78a7eab9353b9a23607
iman-123-coder <imanabbasi101@gmail.com>
2026-05-15 20:55:17 +0500

    fix home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/products" element={<ProductListing />} />
